d32bf63681f9a66b72b739fd611e1ee84639aa38
kimwoojin <kimwoojin1903@gmail.com>
2024-01-25 12:48:22 +0900

    아이디어 작성 페이지 마크업

--- app/ideaApply/ideaWrite/page.tsx
+++ app/ideaApply/ideaWrite/page.tsx
@@ -9,11 +9,18 @@ export default function IdeaWrite(){
   const handleEditorChange = (value : string) => {
     setEditorValue(value);
   };
+
   return (
     <div>
       <div className='flex flex-col justify-center items-center h-screen'>
-      <h1 className='font-bold text-3xl mb-5'>아이디어 작성</h1>
-        <TextEditor value={editorValue} onChange={handleEditorChange} />    
+        <h1 className='font-bold text-3xl mb-5'>아이디어 작성</h1>
+        <TextEditor value={editorValue} onChange={handleEditorChange} />
+      </div>
+      <div className='flex justify-center relative left-72 bottom-14'>
+        <div className='flex w-56 justify-between'>
+          <button className='w-24 h-10 bg-green-500 text-white rounded-lg'>저장하기</button>
+          <button className='w-24 h-10 bg-green-500 text-white rounded-lg'>제출하기</button>
+        </div>
       </div>
     </div>
   )
