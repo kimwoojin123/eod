841dacf8b2a3751bfd55b330934945db1f6028fc
kimwoojin <kimwoojin1903@gmail.com>
2024-01-25 11:57:50 +0900

    아이디어 추가 버튼 링크 생성

--- app/ui/Buttons/ideaApplyButtons.tsx
+++ app/ui/Buttons/ideaApplyButtons.tsx
@@ -1,12 +1,11 @@
+import Link from 'next/link'
+
 export function IdeaWriteButton(){
-  return (
-    <button className="bg-gray-300 w-80 h-80 rounded-xl">아이디어 작성</button>
-  )
+  return <Link href="/ideaApply/ideaWrite" className="bg-gray-300 w-80 h-80 rounded-xl"><p className='flex h-full justify-center items-center'>아이디어 작성</p></Link>
+
 }
 
-
 export function IdeaDrawButton(){
-  return (
-    <button className="bg-gray-300 w-80 h-80 rounded-xl">아이디어 작성</button>
-  )
+  return <Link href="/ideaApply/ideaDraw" className="bg-gray-300 w-80 h-80 rounded-xl"><p className='flex h-full justify-center items-center'>아이디어 그리기</p></Link>
+
 }
