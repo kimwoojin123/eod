86598ab9a4eb0234a97aa66e8d5a99e7f009447a
kimwoojin <kimwoojin1903@gmail.com>
2024-01-31 14:33:03 +0900

    경로변경

---
+++ app/ui/ideaWrite/textEditor.tsx
@@ -0,0 +1,33 @@
+import dynamic from 'next/dynamic';
+
+const DynamicQuill = dynamic(() => import('react-quill'), { ssr: false });
+import 'react-quill/dist/quill.snow.css';
+
+interface TextEditorProps {
+  value: string;
+  onChange: (value: string) => void;
+}
+
+const TextEditor: React.FC<TextEditorProps> = ({ value, onChange }) => {
+  const modules = {
+    toolbar: {
+      container: [
+        ["image", "link"],
+        [{ header: [1, 2, 3, 4, 5, false] }],
+        ["bold", "italic", "underline", "strike"],
+      ],
+    },
+  };
+
+  return (
+    <DynamicQuill
+      style={{ width: "800px", height: "600px" }}
+      theme="snow"
+      value={value}
+      onChange={onChange}
+      modules={modules}
+    />
+  );
+};
+
+export default TextEditor;
