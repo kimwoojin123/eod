dd82705e3abe027071b79581b7c53e1c13227aab
kimwoojin <kimwoojin1903@gmail.com>
2024-01-31 14:58:38 +0900

    삭제 모달 설정

--- app/ideaSearch/[id]/page.tsx
+++ app/ideaSearch/[id]/page.tsx
@@ -5,6 +5,8 @@ import { usePathname } from 'next/navigation';
 import Image from 'next/image';
 import BackButton from '@/app/ui/Buttons/backButton';
 import Delete from '@/app/ui/ideaSearch/delete'
+import Modal from 'react-modal'
+import { useRouter } from 'next/navigation'
 
 interface DetailPageProps {
   username: string;
@@ -17,9 +19,16 @@ interface DetailPageProps {
 export default function DetailPage() {
   const [boardData, setBoardData] = useState<DetailPageProps | null>(null);
   const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [message, setMessage] = useState<string>('');
+  const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
 
   const searchPath = usePathname();
+  const router = useRouter();
   const id = searchPath.split('/').pop();
+  
+
+  const openModal = () => setModalIsOpen(true);
+  const closeModal = () => {setModalIsOpen(false); router.back()}
 
   useEffect(() => {
     const fetchData = async () => {
@@ -48,9 +57,11 @@ export default function DetailPage() {
       });
   
       if (response.ok) {
-        console.log('데이터가 성공적으로 삭제되었습니다.');
+        setMessage('삭제가 완료되었습니다.')
+        openModal()
       } else {
-        console.error('데이터를 삭제하는 중에 오류가 발생했습니다.');
+        setMessage('삭제에 실패했습니다.')
+        openModal()
       }
     } catch (error) {
       console.error('Error deleting data:', error);
@@ -79,6 +90,44 @@ export default function DetailPage() {
         {showDeleteModal && (
           <Delete onDelete={handleDelete} onClose={handleCloseDeleteModal} />
         )}
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        ariaHideApp={false}
+        style={{
+          overlay: {
+            backgroundColor: " rgba(0, 0, 0, 0.4)",
+            width: "100%",
+            height: "100vh",
+            zIndex: "10",
+            position: "fixed",
+            top: "0",
+            left: "0",
+          },
+          content: {
+            display:"flex",
+            flexDirection : "column",
+            alignItems : 'center',
+            width: "360px",
+            height: "180px",
+            zIndex: "150",
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            borderRadius: "10px",
+            boxShadow: "2px 2px 2px rgba(0, 0, 0, 0.25)",
+            backgroundColor: "white",
+            justifyContent: "center",
+            overflow: "auto",
+            whiteSpace: 'pre-line',
+          },
+        }}
+        contentLabel="삭제 완료 모달"
+      >
+        <p>{message}</p>
+        <button className="w-40 h-10 rounded-2xl bg-gray-200 mt-5" onClick={closeModal}>닫기</button>
+      </Modal>
     </div>
   );
   }
