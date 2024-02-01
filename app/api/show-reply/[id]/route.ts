0e66c5117f4f1b15dff3f0799a651a129d890259
kimwoojin <kimwoojin1903@gmail.com>
2024-02-01 16:02:19 +0900

    댓글 등록, 조회 기능

--- app/api/show-reply/[id]/route.ts
+++ app/api/show-reply/[id]/route.ts
@@ -1,6 +1,5 @@
 import { connectDB, closeConnection, client } from '../../../../utills/db';
 import { NextRequest, NextResponse } from 'next/server';
-import { ObjectId } from 'mongodb'; // Import ObjectId from mongodb
 
 export async function GET(req: NextRequest) {
   try {
@@ -8,14 +7,13 @@ export async function GET(req: NextRequest) {
 
     const path = req.nextUrl.pathname;
     const id = path.split('/').pop();
-    console.log(id)
-    const idea = await client.db('eoddb').collection('reply').findOne({ ideaId : new ObjectId(id) });
-
-    if (!idea) {
+    
+    const replies = await client.db('eoddb').collection('reply').find({ ideaId : id }).toArray();
+    if (!replies) {
       return NextResponse.json({ error: '해당하는 댓글목록을 찾을 수 없습니다.' }, { status: 404 });
     }
 
-    return NextResponse.json(idea, { status: 200 });
+    return NextResponse.json(replies, { status: 200 });
   } catch (error) {
     console.error('Error fetching individual idea:', error);
     return NextResponse.json({ error: '댓글 정보를 불러오는 중에 오류가 발생했습니다.' }, { status: 500 });
