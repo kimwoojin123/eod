6285fbc17743880efb8b1737d0c209e9946c4d6c
kimwoojin <kimwoojin1903@gmail.com>
2024-02-01 16:59:10 +0900

    댓글작성시 모달창 추가, 댓글목록 업데이트

--- app/ui/ideaSearch/reply.tsx
+++ app/ui/ideaSearch/reply.tsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 import { getUsernameSomehow } from '../getUsername';
+import Modal from 'react-modal'
+
 
 interface ReplyProps {
   _id: string;
@@ -16,20 +18,24 @@ export default function Reply({ ideaId }: { ideaId: string }) {
     username: '',
     textContent: '',
   });
+  const [message, setMessage] = useState<string>('');
+  const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
 
+  const openModal = () => setModalIsOpen(true);
+  const closeModal = () => {setModalIsOpen(false); fetchReplies()}
+
+  const fetchReplies = async () => {
+    try {
+      const response = await fetch(`/api/show-reply/${ideaId}`);
+      const data = await response.json();
+      setReplies(data);
+    } catch (error) {
+      console.error('Error fetching replies:', error);
+    }
+  };
 
   useEffect(() => {
-    const fetchReplies = async () => {
-      try {
-        const response = await fetch(`/api/show-reply/${ideaId}`);
-        const data = await response.json();
-        setReplies(data);
-      } catch (error) {
-        console.error('Error fetching replies:', error);
-      }
-    };
-  
-    if (ideaId) {
+      if (ideaId) {
       fetchReplies();
     }
   }, [ideaId]);
@@ -47,6 +53,8 @@ export default function Reply({ ideaId }: { ideaId: string }) {
 
       if (response.ok) {
         setNewReply({ ...newReply, textContent: '' });
+        setMessage('댓글이 등록되었습니다.')
+        openModal()
       } else {
         console.error('Error adding reply:', response.statusText);
       }
@@ -85,6 +93,44 @@ export default function Reply({ ideaId }: { ideaId: string }) {
           <button onClick={handleAddReply} className='border ml-2'>등록</button>
         </div>
       </div>
+      <Modal
+            isOpen={modalIsOpen}
+            onRequestClose={closeModal}
+            ariaHideApp={false}
+            style={{
+              overlay: {
+                backgroundColor: " rgba(0, 0, 0, 0.4)",
+                width: "100%",
+                height: "100vh",
+                zIndex: "10",
+                position: "fixed",
+                top: "0",
+                left: "0",
+              },
+              content: {
+                display:"flex",
+                flexDirection : "column",
+                alignItems : 'center',
+                width: "360px",
+                height: "180px",
+                zIndex: "150",
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                borderRadius: "10px",
+                boxShadow: "2px 2px 2px rgba(0, 0, 0, 0.25)",
+                backgroundColor: "white",
+                justifyContent: "center",
+                overflow: "auto",
+                whiteSpace: 'pre-line',
+              },
+            }}
+            contentLabel="댓글 등록 모달"
+          >
+            <p>{message}</p>
+            <button className="w-40 h-10 rounded-2xl bg-gray-200 mt-5" onClick={closeModal}>닫기</button>
+          </Modal>
     </div>
   );
             }
