c56cf7f82d4d51f9a38e0beae9d83fc5042469be
kimwoojin <kimwoojin1903@gmail.com>
2024-02-06 15:55:53 +0900

    팀 상세 페이지

---
+++ app/api/team-detail/[id]/route.ts
@@ -0,0 +1,25 @@
+import { connectDB, closeConnection, client } from '../../../../utills/db';
+import { NextRequest, NextResponse } from 'next/server';
+import { ObjectId } from 'mongodb'; // Import ObjectId from mongodb
+
+export async function GET(req: NextRequest) {
+  try {
+    await connectDB();
+
+    const path = req.nextUrl.pathname;
+    const id = path.split('/').pop();
+
+    const team = await client.db('eoddb').collection('teams').findOne({ _id: new ObjectId(id) });
+
+    if (!team) {
+      return NextResponse.json({ error: '해당하는 팀를 찾을 수 없습니다.' }, { status: 404 });
+    }
+
+    return NextResponse.json(team, { status: 200 });
+  } catch (error) {
+    console.error('Error fetching individual idea:', error);
+    return NextResponse.json({ error: '팀 정보를 불러오는 중에 오류가 발생했습니다.' }, { status: 500 });
+  } finally {
+    await closeConnection();
+  }
+}
---
+++ app/teamSearch/[id]/page.tsx
@@ -0,0 +1,61 @@
+'use client'
+
+import { useEffect, useState } from 'react';
+import { usePathname } from 'next/navigation';
+import Image from 'next/image'
+
+interface Team {
+  username: string;
+  name: string;
+  lang: string;
+  purpose: string;
+  headCount: string;
+  imageUrl: string;
+}
+
+export default function TeamDetail() {
+  const idPath = usePathname();
+  const id = idPath.split('/').pop();
+
+  const [team, setTeam] = useState<Team | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchTeam = async () => {
+      try {
+        const response = await fetch(`/api/team-detail/${id}`);
+        if (!response.ok) {
+          throw new Error('Failed to fetch team data');
+        }
+        const data = await response.json();
+        setTeam(data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching team data:', error);
+      }
+    };
+
+    if (id) {
+      fetchTeam();
+    }
+  }, [id]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!team) {
+    return <div>Team not found</div>;
+  }
+
+  return (
+    <div>
+      <h1>{team.name}</h1>
+      <Image src={team.imageUrl} alt={`${team.name} 이미지`} width={500} height={500}  />
+      <p>팀장: {team.username}</p>
+      <p>팀 목표: {team.purpose}</p>
+      <p>개발 언어: {team.lang}</p>
+      <p>모집 인원: {team.headCount}</p>
+    </div>
+  );
+}
