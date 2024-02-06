75a78677cd46388f0aa10136528aa1e1abc07bf6
kimwoojin <kimwoojin1903@gmail.com>
2024-02-06 15:45:31 +0900

    css변경

--- app/teamSearch/page.tsx
+++ app/teamSearch/page.tsx
@@ -2,6 +2,7 @@
 
 import Link from 'next/link'
 import { useState, useEffect } from 'react'
+import Image from 'next/image'
 
 interface Team {
   _id: string;
@@ -32,17 +33,16 @@ export default function TeamSearch(){
 
   return (
     <div>
-      <Link href="/teamSearch/teamApply">
+      <Link href="/teamSearch/teamApply" className='flex justify-end mt-12 mr-40'>
         <p className="flex justify-center items-center w-32 h-10 text-white bg-green-500 rounded-2xl">
           팀 등록하기
         </p>
       </Link>
-      <h2 className="text-2xl font-bold mt-4 mb-2">팀 목록</h2>
-
-      <div className="grid grid-cols-3 gap-4">
+      <h2 className="text-2xl font-bold mt-4 mb-2 ml-20">팀 목록</h2>
+      <div className="grid grid-cols-6 gap-4 pl-20 pr-20 pt-5">
         {teams.map((team) => (
           <div key={team._id} className="border p-4 rounded-md">
-            <img src={team.imageUrl} alt={`${team.name} 이미지`} className="w-full h-40 object-cover mb-2 rounded-md" />
+            <Image src={team.imageUrl} alt={`${team.name} 이미지`} width={800} height={800} className="w-full h-40 object-contain mb-2 rounded-md" />
             <h3 className="text-lg font-semibold mb-2">{team.name}</h3>
             <p className="text-gray-500 mb-2">팀장: {team.username}</p>
             <p className="text-gray-500 mb-2">개발 언어: {team.lang}</p>
