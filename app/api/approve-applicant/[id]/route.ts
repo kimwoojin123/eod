d5aa031072a93969516516df4a435ae6eafa1367
kimwoojin <kimwoojin1903@gmail.com>
2024-02-07 17:17:00 +0900

    변경내용 추가

--- app/api/approve-applicant/[id]/route.ts
+++ app/api/approve-applicant/[id]/route.ts
@@ -9,16 +9,15 @@ export async function POST(req: NextRequest) {
     const {team_id} = jsonData
     const idPath = req.nextUrl.pathname;
     const user_id = idPath.split('/').pop();
-    console.log(user_id);
-    console.log(team_id);
-    
+
     if(!user_id){
       throw new Error('Invalid id format');
     }
 
     await addTeamMember(team_id, user_id);
+    const approvedState= await approveApplicant(team_id, user_id)
 
-    return NextResponse.json({ message: '지원자가 성공적으로 승인되었습니다.' }, { status: 200 });
+    return NextResponse.json({ approvedState }, { status: 200 });
   } catch (error) {
     console.error('지원자 승인 중 오류가 발생했습니다:', error);
     return NextResponse.json({ error: '지원자 승인 중 오류가 발생했습니다.' }, { status: 500 });
@@ -31,5 +30,12 @@ async function addTeamMember(team_id: string, user_id: string) {
   await client.db('eoddb').collection('teams').updateOne(
     { _id: new ObjectId(team_id) },
     { $addToSet: { teamMember: user_id } }
+  );
+}
+
+async function approveApplicant(team_id: string, user_id: string) {
+  await client.db('eoddb').collection('teamApply').updateOne(
+    { team_id: new ObjectId(team_id), user_id: new ObjectId(user_id) },
+    { $set: { approved: true } }
   );
 }
--- app/mypage/teamManage/page.tsx
+++ app/mypage/teamManage/page.tsx
@@ -25,6 +25,7 @@ interface Applicant {
   content: string;
   team_id: string;
   user_info:UserInfo;
+  approved:boolean;
 }
 
 interface UserInfo{
@@ -38,6 +39,9 @@ export default function TeamManage() {
   const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
   const [applicants, setApplicants] = useState<Applicant[]>([]);
   const [selectedApplicant, setSelectedApplicant] = useState<Applicant | null>(null);
+  const [approvalModalIsOpen, setApprovalModalIsOpen] = useState<boolean>(false); 
+  const [approvalClicked, setApprovalClicked] = useState<boolean>(false); // 승인 버튼 클릭 상태
+
 
   useEffect(() => {
     async function fetchTeamData() {
@@ -79,6 +83,9 @@ export default function TeamManage() {
     setModalIsOpen(false);
     setApplicants([]);
   };
+
+  
+
   const approve = async (applicantId: string, teamId: string) => {
     try {
       const response = await fetch(`/api/approve-applicant/${applicantId}`, {
@@ -92,9 +99,9 @@ export default function TeamManage() {
       if (!response.ok) {
         throw new Error('지원자 승인에 실패했습니다.');
       }
+      setApprovalModalIsOpen(true);
+      setApprovalClicked(true);
 
-      console.log('지원자가 성공적으로 승인되었습니다.');
-  
     } catch (error) {
       console.error('지원자 승인 중 오류 발생:', error);
     }
@@ -103,6 +110,41 @@ export default function TeamManage() {
   const handleDetailClick = (applicant: Applicant) => {
     setSelectedApplicant(applicant === selectedApplicant ? null : applicant);
   };
+
+
+  useEffect(() => {
+    const fetchApprovalStatus = async () => {
+      try {
+        if (!selectedApplicant) return; // 선택된 지원자가 없으면 함수 종료
+  
+        const response = await fetch(`/api/get-approve`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ team_id: selectedApplicant.team_id }), 
+        });
+  
+        if (!response.ok) {
+          throw new Error('Failed to fetch approval status');
+        }
+  
+        const data = await response.json();
+        const approvedState = data.approved; 
+  
+        setSelectedApplicant(prevApplicant => ({
+          ...prevApplicant!,
+          approved: approvedState
+        }));
+      } catch (error) {
+        console.error('Failed to fetch approval status:', error);
+      }
+    };
+  
+    fetchApprovalStatus();
+  }, []);
+
+
 
   return (
     <div>
@@ -156,28 +198,74 @@ export default function TeamManage() {
           <div className="modal-content">
             <h2>지원자 정보</h2>
             <ul>
-              {applicants.map((applicant) => (
-                <li key={applicant._id}>
-                  <div className="flex justify-between items-center w-full">
-                    <p>유저명 : {applicant.username}</p>
-                    <button className="w-32 h-10 rounded-2xl bg-gray-200 ml-5" onClick={() => handleDetailClick(applicant)}>자세히 보기</button>
-                  </div>
-                  {selectedApplicant === applicant && (
-                      <div className="flex flex-col mt-3">
-                      <p>이름 : {applicant.name}</p>
-                      <p>이메일 : {applicant.email}</p>
-                      <p>전화번호 : {applicant.phoneNumber}</p>
-                      <p>스택: {applicant.stack}</p>
-                      <p>지원내용: {applicant.content}</p>
+            {applicants.map((applicant) => (
+              <li key={applicant._id}>
+                <div className="flex justify-between items-center w-full mb-4">
+                  <p>유저명 : {applicant.username}</p>
+                  <button className="w-32 h-8 rounded-2xl bg-gray-200 ml-5" onClick={() => handleDetailClick(applicant)}>자세히 보기</button>
+                </div>
+                {selectedApplicant === applicant && (
+                  <div className="flex flex-col mt-3 mb-4">
+                    <p>이름 : {applicant.name}</p>
+                    <p>이메일 : {applicant.email}</p>
+                    <p>전화번호 : {applicant.phoneNumber}</p>
+                    <p>스택: {applicant.stack}</p>
+                    <p>지원내용: {applicant.content}</p>
+                    {applicant.approved ? (
+                    <div className="flex justify-center items-center ml-16 w-40 h-10 rounded-2xl bg-gray-200 mt-5">승인완료</div>
+                     ) : (
                       <button className="ml-16 w-40 h-10 rounded-2xl bg-green-200 mt-5" onClick={() => approve(applicant._id, applicant.team_id)}>승인</button>
-                    </div>
-                  )}
-                </li>
+                    )}
+                  </div>
+                )}
+              </li>
               ))}
             </ul>
           </div>
         </div>
         <button className="w-40 h-10 rounded-2xl bg-gray-200 mt-5" onClick={closeModal}>닫기</button>
+      </Modal>
+      <Modal
+        isOpen={approvalModalIsOpen}
+        onRequestClose={() => setApprovalModalIsOpen(false)}
+        ariaHideApp={false}
+        style={{
+          overlay: {
+            backgroundColor: " rgba(0, 0, 0, 0.4)",
+            width: "100%",
+            height: "100vh",
+            zIndex: "10",
+            position: "fixed",
+            top: "0",
+            left: "0",
+          },
+          content: {
+            display: "flex",
+            flexDirection: "column",
+            alignItems: 'center',
+            width: "420px",
+            height: "200px",
+            zIndex: "150",
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            borderRadius: "10px",
+            boxShadow: "2px 2px 2px rgba(0, 0, 0, 0.25)",
+            backgroundColor: "white",
+            justifyContent: "center",
+            overflow: "auto",
+            whiteSpace: 'pre-line',
+          },
+        }}
+        contentLabel="승인 완료 모달"
+      >
+        <div className="modal">
+          <div className="modal-content">
+            <h2>승인이 완료되었습니다!</h2>
+            <button className="w-40 h-10 rounded-2xl bg-gray-200 mt-5" onClick={() => setApprovalModalIsOpen(false)}>확인</button>
+          </div>
+        </div>
       </Modal>
     </div>
   );
