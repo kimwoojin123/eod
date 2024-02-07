4a825a2954c75c34842bc94fe652bc2a75210212
kimwoojin <kimwoojin1903@gmail.com>
2024-02-07 17:41:22 +0900

    남은인원 표시

--- app/teamSearch/[id]/page.tsx
+++ app/teamSearch/[id]/page.tsx
@@ -12,6 +12,8 @@ interface Team {
   purpose: string;
   headCount: string;
   imageUrl: string;
+  teamMember: string[]; 
+
 }
 
 export default function TeamDetail() {
@@ -49,15 +51,20 @@ export default function TeamDetail() {
     return <div>Team not found</div>;
   }
 
+  const remainingMembers = parseInt(team.headCount) - team.teamMember.length;
+
   return (
-    <div>
-      <h1>{team.name}</h1>
-      <Image src={team.imageUrl} alt={`${team.name} 이미지`} width={500} height={500}  />
-      <p>팀장: {team.username}</p>
-      <p>팀 목표: {team.purpose}</p>
-      <p>개발 언어: {team.lang}</p>
-      <p>모집 인원: {team.headCount}</p>
-      <Link href={`/teamSearch/${id}/teamApply`}>팀 지원하기</Link>
+    <div className="flex flex-col w-screen h-screen items-center justify-center">
+      <h1 className="text-2xl font-bold mb-4">{team.name}</h1>
+      <div className="w-64 h-64 mb-4">
+        <Image src={team.imageUrl} alt={`${team.name} 이미지`} width={500} height={500} />
+      </div>
+      <p className="mb-2">팀장: {team.username}</p>
+      <p className="mb-2">팀 목표: {team.purpose}</p>
+      <p className="mb-2">개발 언어: {team.lang}</p>
+      <p className="mb-2">모집 인원: {team.headCount}명</p>
+      <p className="mb-2">남은 인원: {remainingMembers}명</p>
+      <Link href={`/teamSearch/${id}/teamApply`} className="text-blue-500">팀 지원하기</Link>
     </div>
   );
 }
