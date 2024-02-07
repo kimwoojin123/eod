ddb633e005808f9f37a2abc7a0f3c3f9744091ae
kimwoojin <kimwoojin1903@gmail.com>
2024-02-07 13:10:22 +0900

    팀 지원정보 보기

---
+++ app/api/inquire-apply/[id]/route.ts
@@ -0,0 +1,33 @@
+import { connectDB, closeConnection, client } from '../../../../utills/db'
+import { NextRequest, NextResponse } from 'next/server';
+import { ObjectId } from 'mongodb';
+
+interface Applicant{
+  user_id: ObjectId;
+}
+
+export async function GET(req: NextRequest) {
+  try {
+    await connectDB();
+    const idPath = req.nextUrl.pathname;
+    const teamIdString = idPath.split('/').pop();
+    const teamId = new ObjectId(teamIdString);
+
+    const applicants = await client.db('eoddb').collection('teamApply').find({ team_id: teamId }).toArray();
+      
+    const promises = applicants.map(async (applicant : Applicant) => {
+      const user = await client.db('eoddb').collection('users').findOne({ _id: applicant.user_id });
+      return {
+        ...applicant,
+        user_info: user // 지원자 정보와 함께 사용자 정보도 반환
+       };
+     });
+
+      const applicantsWithUserInfo = await Promise.all(promises);
+
+      return NextResponse.json({ applicants: applicantsWithUserInfo }, { status: 200 });
+    } catch (error) {
+      console.error('지원자 정보 조회 중 오류 발생:', error);
+      return NextResponse.json({ error: '지원자 정보를 불러오는 중에 오류가 발생했습니다.' }, { status: 500 });
+    } 
+  }
--- app/mypage/teamManage/page.tsx
+++ app/mypage/teamManage/page.tsx
@@ -4,7 +4,7 @@ import { getUsernameSomehow } from '@/app/ui/getUsername';
 import { useState, useEffect } from 'react';
 
 interface Team {
-  _id : string;
+  _id: string;
   username: string;
   name: string;
   lang: string;
@@ -13,8 +13,27 @@ interface Team {
   imageUrl: string;
 }
 
+interface Applicant {
+  _id: string;
+  name : string;
+  username: string;
+  email : string;
+  phoneNumber:string;
+  stack: string;
+  content: string;
+  user_info:UserInfo;
+}
+
+interface UserInfo{
+  name : string;
+  username: string;
+  email : string;
+  phoneNumber:string;
+}
 export default function TeamManage() {
   const [teams, setTeams] = useState<Team[]>([]);
+  const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
+  const [applicants, setApplicants] = useState<Applicant[]>([]);
 
   useEffect(() => {
     async function fetchTeamData() {
@@ -34,6 +53,28 @@ export default function TeamManage() {
     fetchTeamData();
   }, []);
 
+  const openModal = async (teamId: string) => {
+    try {
+      const response = await fetch(`/api/inquire-apply/${teamId}`);
+      if (!response.ok) {
+        throw new Error('지원자 정보를 불러오는데 문제가 발생했습니다.');
+      }
+      const applicantData = await response.json();
+      setApplicants(applicantData.applicants.map((applicant : Applicant ) => ({
+        ...applicant,
+        ...applicant.user_info
+      })));
+      setModalIsOpen(true);
+    } catch (error) {
+      console.error('지원자 정보를 불러오는데 문제가 발생했습니다:', error);
+    }
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+    setApplicants([]);
+  };
+
   return (
     <div>
       <h1>내 팀 관리</h1>
@@ -41,11 +82,32 @@ export default function TeamManage() {
         {teams.map((team) => (
           <li key={team._id} className='flex'>
             <h2>{team.name}</h2>
-            <button className='ml-40 mr-20'>지원신청 보기</button>
+            <button className='ml-40 mr-20' onClick={() => openModal(team._id)}>지원신청 보기</button>
             <button>팀 정보 수정</button>
           </li>
         ))}
       </ul>
+
+      {modalIsOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={closeModal}>&times;</span>
+            <h2>지원자 정보</h2>
+            <ul>
+              {applicants.map((applicant) => (
+                <li key={applicant._id}>
+                  <p>이름 : {applicant.name}</p>
+                  <p>유저명 : {applicant.username}</p>
+                  <p>이메일 : {applicant.email}</p>
+                  <p>전화번호 : {applicant.phoneNumber}</p>
+                  <p>스택: {applicant.stack}</p>
+                  <p>지원내용: {applicant.content}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
