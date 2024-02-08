69dcf400e5ec0ec27ae511a9380a30f2dcb57ac4
kimwoojin <kimwoojin1903@gmail.com>
2024-02-08 17:21:30 +0900

    지원자 많은 팀 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Category from "./ui/category";
 import PopularIdeas from "./ui/main/popularIdeas";
+import PopularTeams from "./ui/main/popularTeams";
 
 export default function Home() {
   return (
     <div className='w-screen'>
       <div><Category /></div>
-      <div><PopularIdeas /></div>
+      <div className="flex">
+      <PopularIdeas />
+      <PopularTeams />
+      </div>
     </div>
   );
 }
---
+++ app/ui/main/popularTeams.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { UserPlusIcon } from '@heroicons/react/24/solid';
+
+interface Team {
+  _id: string;
+  name: string;
+  applicants: number;
+}
+
+export default function PopularTeams() {
+  const [teams, setTeams] = useState<Team[]>([]);
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchPopularTeams = async () => {
+      try {
+        const response = await fetch('/api/get-teamApply');
+        if (!response.ok) {
+          throw new Error('Failed to fetch popular teams');
+        }
+        const data = await response.json();
+        setTeams(data);
+      } catch (error) {
+        console.error('Error fetching popular teams:', error);
+      }
+    };
+
+    fetchPopularTeams();
+  }, []);
+
+  const handleTeamClick = (teamId: string) => {
+    router.push(`/teamSearch/${teamId}`);
+  };
+
+  return (
+    <div className='w-1/5 mt-10'>
+      <h1 className="text-xl font-bold mb-4">지원자 많은 팀</h1>
+      <div className='border border-gray-300 p-4' style={{ height: '580px' }}>
+        {teams.map((team, index) => (
+          <div key={team._id} className="py-2 cursor-pointer" onClick={() => handleTeamClick(team._id)}>
+            <p className="hover:underline flex">{`${index + 1}. ${team.name}`}<UserPlusIcon className='w-5 h-5 ml-2 mt-0.5'/> {team.applicants}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
