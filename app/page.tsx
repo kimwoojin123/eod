6c63651a57d7df798cc4b3aed1ab66bf8ba1403b
kimwoojin <kimwoojin1903@gmail.com>
2024-02-08 16:50:02 +0900

    인기 아이디어 컴포넌트 작성

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Category from "./ui/category";
+import PopularIdeas from "./ui/main/popularIdeas";
 
 export default function Home() {
   return (
     <div className='w-screen'>
       <div><Category /></div>
+      <div><PopularIdeas /></div>
     </div>
   );
 }
---
+++ app/ui/main/popularIdeas.tsx
@@ -0,0 +1,61 @@
+'use client'
+
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { HeartIcon } from '@heroicons/react/24/solid';
+import { ChatBubbleOvalLeftEllipsisIcon } from '@heroicons/react/24/outline';
+
+interface Idea {
+  _id: string;
+  username: string;
+  title: string;
+  likes:number;
+  replies:string;
+}
+
+export default function PopularIdeas() {
+  const [ideas, setIdeas] = useState<Idea[]>([]);
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchPopularIdeas = async () => {
+      try {
+        const response = await fetch('/api/board-list');
+        if (!response.ok) {
+          throw new Error('Failed to fetch popular ideas');
+        }
+        const data = await response.json();
+        if (data && Array.isArray(data)) {
+          const sortedIdeas = data.sort((a: Idea, b: Idea) => b.likes - a.likes).slice(0, 10);
+          setIdeas(sortedIdeas);
+        } else {
+          throw new Error('Invalid data structure: ideas field not found');
+        }
+      } catch (error) {
+        console.error('Error fetching popular ideas:', error);
+      }
+    };
+
+    fetchPopularIdeas();
+  }, []);
+
+  const handleIdeaClick = (id: string) => {
+    router.push(`/ideaSearch/${id}`);
+  };
+
+  return (
+    <div className='w-1/5 ml-20 mt-10'>
+      <h1 className="text-xl font-bold mb-4">인기 아이디어</h1>
+      <div className='border border-gray-300 p-4 flex flex-col justify-start' style={{ height: '580px' }}>
+      {ideas.map((idea, index) => (
+        <div key={idea._id} className="py-2 cursor-pointer " onClick={() => handleIdeaClick(idea._id)}>
+          <p className="flex hover:underline">{`${index + 1}. ${idea.username} - ${idea.title}`}
+          <HeartIcon className='w-4 h-4 text-red-500 ml-2 mt-1'/>{idea.likes}
+          <ChatBubbleOvalLeftEllipsisIcon className='w-4 h-4 ml-3 mt-1'/>{idea.replies?.length || 0}
+        </p>
+        </div>
+      ))}
+      </div>
+    </div>
+  );
+}
