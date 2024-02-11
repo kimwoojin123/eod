eedbce27dec6fa915f3c2b1a6ac7dd1c274fc438
kimwoojin <kimwoojin1903@gmail.com>
2024-02-12 00:26:58 +0900

    서버 엔트리포인트

---
+++ app/api/team-idea-apply/route.ts
@@ -0,0 +1,28 @@
+import { connectDB, closeConnection, client } from '../../../utills/db'
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(req: NextRequest) {
+  try {
+    const { username, teamId, content } = req.body;
+    await connectDB();
+
+    // ideaApply 컬렉션에 데이터 추가
+    const response = await client.db('eoddb').collection('ideaApply').insertOne({
+      idea_id: req.query.ideaId, // 클라이언트에서 전달된 아이디어 ID를 사용
+      content,
+      developer: teamId,
+      username,
+    });
+
+    if (response.insertedId) {
+      return NextResponse.json({ message: '아이디어 지원이 성공적으로 제출되었습니다.' }, { status: 200 });
+    } else {
+      return NextResponse.json({ error: '아이디어 지원 제출에 실패했습니다.' }, { status: 500 });
+    }
+  } catch (error) {
+    console.error('Error submitting idea support:', error);
+    return NextResponse.json({ error: '아이디어 지원 제출에 실패했습니다.' }, { status: 500 });
+  } finally {
+    await closeConnection();
+  }
+}
