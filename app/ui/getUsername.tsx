079c3124413b078eed8c74e470f6bd5c9cf90fb5
kimwoojin <kimwoojin1903@gmail.com>
2024-02-22 17:30:00 +0900

    localstoage 클라이언트에서만 사용 제한

--- app/ui/getUsername.tsx
+++ app/ui/getUsername.tsx
@@ -1,18 +1,20 @@
-import { decode } from "js-base64"
+import { decode } from "js-base64";
 
 export function getUsernameSomehow() {
-  const token = localStorage.getItem("token");
-  
-  if (token) {
-    try {
-      const payload = token.split('.')[1];
-      const decodedPayload = decode(payload);
-      const payloadObject = JSON.parse(decodedPayload);
-      return payloadObject.username;
-    } catch (error) {
-      console.error("Error parsing token:", error);
+  if (typeof window !== 'undefined') {
+    const token = localStorage.getItem("token");
+    
+    if (token) {
+      try {
+        const payload = token.split('.')[1];
+        const decodedPayload = decode(payload);
+        const payloadObject = JSON.parse(decodedPayload);
+        return payloadObject.username;
+      } catch (error) {
+        console.error("Error parsing token:", error);
+      }
     }
   }
 
   return null;
-};
+}
