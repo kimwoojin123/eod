27a41d1c699d50066e2f80d817136f72a8bae517
kimwoojin <kimwoojin1903@gmail.com>
2024-02-23 11:52:17 +0900

    데이터 읽기방식 변경

--- app/api/team-list/route.ts
+++ app/api/team-list/route.ts
@@ -1,6 +1,7 @@
 import { connectDB, closeConnection, client } from '../../../utills/db';
 import { NextRequest, NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
 
 export async function GET(){
   try{
