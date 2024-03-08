a59dc5ba16c945836a203fe1d9239a95f4a3054a
kimwoojin <kimwoojin1903@gmail.com>
2024-03-08 11:24:48 +0900

    검색기능 디버깅

--- app/api/idea/search/[searchOption]/[searchText]/route.ts
+++ app/api/idea/search/[searchOption]/[searchText]/route.ts
@@ -1,34 +1,32 @@
-import { connectDB, closeConnection, client } from '../../../../../../utills/db';
-import { NextRequest, NextResponse } from 'next/server';
+  import { connectDB, closeConnection, client } from '../../../../../../utills/db';
+  import { NextRequest, NextResponse } from 'next/server';
 
 
-interface Query {
-  [key: string]: { $regex: string; $options: string };
-}
-export const dynamic = 'force-dynamic';
-
-export async function GET(req:NextRequest){
-  try{
-    await connectDB();
-    const path = req.nextUrl.pathname
-    const searchOption = path.split('/')[3]|| ''
-    const searchText = path.split('/').pop()|| ''
-
-  
-    if (searchOption === null) {
-      closeConnection();
-      return NextResponse.json({ error: 'Invalid search option' }, { status: 400 });
-    }
-
-    const query: Query = {};
-    const decodedSearchText = decodeURIComponent(searchText);
-    query[searchOption] = { $regex: decodedSearchText  || '', $options: 'i' };
-    const searchResults = await client.db('eoddb').collection('ideas').find(query).toArray();
-
-    closeConnection();
-    return NextResponse.json({searchResults}, {status: 200})
-  } catch (error) {
-    console.error('Error processing search request:', error);
-    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+  interface Query {
+    [key: string]: { $regex: string; $options: string };
   }
-}
+  export const dynamic = 'force-dynamic';
+
+  export async function GET(req:NextRequest){
+    try{
+      await connectDB();
+      const path = req.nextUrl.pathname
+      const searchOption = path.split('/')[4]|| ''
+      const searchText = path.split('/').pop()|| ''
+      console.log(searchOption)
+      if (searchOption === null) {
+        closeConnection();
+        return NextResponse.json({ error: 'Invalid search option' }, { status: 400 });
+      }
+
+      const query: Query = {};
+      
+      const decodedSearchText = decodeURIComponent(searchText);
+      query[searchOption] = { $regex: decodedSearchText  || '', $options: 'i' };
+      const searchResults = await client.db('eoddb').collection('ideas').find(query).toArray();
+      return NextResponse.json({searchResults}, {status: 200})
+    } catch (error) {
+      console.error('Error processing search request:', error);
+      return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    }
+  }
--- app/ideaSearch/page.tsx
+++ app/ideaSearch/page.tsx
@@ -86,6 +86,7 @@ export default function IdeaSearch(){
       .then((response) => response.json())
       .then((data) => {
         setSearchResults(data.searchResults);
+        console.log(data)
       })
       .catch((error) => console.error('Error fetching search results:', error));
     } else {
